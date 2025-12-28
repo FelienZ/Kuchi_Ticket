484351a58c13a8ffc33db1e834f2c068a1e73f41
FelienZ <rajabfadh@gmail.com>
2025-12-28 17:50:00 +0700

    Implement new Endpoint & handler for some new Features & Modify some endpoints

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"Go_Ticket/handler"
+	DEL "Go_Ticket/handler/DELETE"
+	GET "Go_Ticket/handler/GET"
+	POST "Go_Ticket/handler/POST"
+	PUT "Go_Ticket/handler/PUT"
 	"fmt"
 	"net/http"
 )
@@ -13,11 +16,25 @@ func main() {
 		Addr:    "localhost:8000",
 		Handler: router,
 	}
-	router.HandleFunc("/tickets", handler.GetTickets)
-	router.HandleFunc("/ticket/{id}", handler.GetTicketById)
-	router.HandleFunc("POST /ticket", handler.CreateTicket)
-	router.HandleFunc("POST /ticket/{id}/order", handler.OrderTicket)
+
+	// Route Event
+	router.HandleFunc("/events", GET.GetEvents)
+	router.HandleFunc("/event/{id}", GET.GetEventById)
+	router.HandleFunc("POST /event", POST.CreateEvent)
+	router.HandleFunc("PUT /event/{id}", PUT.UpdateEvent)
+	router.HandleFunc("DELETE /event/{id}", DEL.DeleteEvent)
+	// Route User
+	router.HandleFunc("/users", GET.GetUsers)
+	router.HandleFunc("POST /user", POST.CreateUser)
+	router.HandleFunc("PUT /user/{id}", PUT.UpdateUser)
+	router.HandleFunc("DELETE /user/{id}", DEL.DeleteUser)
+
+	// Route Booking
+	router.HandleFunc("POST /event/{id}/booking", POST.OrderTicket)
+	router.HandleFunc("PUT /event/{eventId}/booking/{bookingId}", PUT.UpdateBooking)
+	router.HandleFunc("DELETE /event/{eventId}/booking/{bookingId}", DEL.DeleteBooking)
 	fmt.Printf("Server Run at: http://%s", server.Addr)
+
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
